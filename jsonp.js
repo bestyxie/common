667d01cad221ceef81a2231b3b33f09467b7db10
besty <bestyxiejiayun@gmail.com>
2018-07-19 15:17:17 +0800

    add post method at jsonp

--- jsonp.js
+++ jsonp.js
@@ -9,22 +9,64 @@ var jsonp = function (opt){
     var default_opt = {
         url: "",
         data: {},
+        method: "get",
         success: function (res){},
         fail: function (err) {}
     }
 
     opt = clone(default_opt, opt);
 
+    if(opt.method.toLowerCase() == "get"){
+        get(opt);
+    }
+    else {
+        post(opt);
+    }
+}
+
+function get(opt){
     var script = document.createElement("script");
 
     window.jsonpCb = function (res) {
         document.body.removeChild(script);
-        delete window.jsonCb;
+        delete window.jsonpCb;
 
         opt.success(res);
     }
 
     script.src = opt.url+"?"+handleData(opt.data)+"&cb=jsonpCb"
+}
+
+function post(opt){
+    var iframe = document.createElement("iframe");
+    iframe.name = "iframePost";
+    iframe.style.display = "none";
+    document.body.appendChild(iframe);
+
+    var form = document.createElement("form");
+    var node = document.createElement("input");
+
+    iframe.addEventListener("load", function (){
+        var res = this.contentDocument.body.innerHTML;
+
+        opt.success(JSON.parse(res));
+    });
+
+    form.action = opt.url;
+    form.method = "post";
+    form.target = iframe.name;
+    for(var name in opt.data){
+        node.name = name;
+        node.value = opt.data[name].toString();
+        form.appendChild(node.cloneNode());
+    }
+
+    form.style.display = "none";
+    document.body.appendChild(form);
+
+    form.submit();
+
+    document.body.removeChild(form)
 }
 
 function handleData(data){
